3d7c6dccaddf23851077c1968abbc602e9daa36e
Matthias Gander <matthias.gander@braineet.com>
2022-12-06 22:33:08 +0100

    feat(style): add style to demineur

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import React from "react";
+import styled from "styled-components";
 import Plate from "./components/Plate";
 
 const App = () => {
-  const [width, setWidth] = React.useState(20);
-  const [height, setHeight] = React.useState(20);
+  const [width, setWidth] = React.useState(25);
+  const [height, setHeight] = React.useState(35);
   const [bombs, setBombs] = React.useState(100);
 
   return (
-    <div style={{ display: "flex" }}>
-      <Plate width={width} height={height} bombs={bombs} />
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <label htmlFor="width">Largeur : </label>
-        <input
-          id="width"
-          type="number"
-          value={width}
-          onChange={(e) => setWidth(e.target.value)}
-        />
-        <label htmlFor="height">Hauteur : </label>
-        <input
-          id="height"
-          type="number"
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-        />
-        <label htmlFor="bombs">Bombes : </label>
-        <input
-          id="bombs"
-          type="number"
-          value={bombs}
-          onChange={(e) => setBombs(e.target.value)}
-        />
+    <Container style={{ display: "flex" }}>
+      <div style={{ display: "flex", margin: "8px" }}>
+        <InputWrapper>
+          <label htmlFor="width">Largeur : </label>
+          <input
+            id="width"
+            type="number"
+            value={width}
+            onChange={(e) => setWidth(e.target.value)}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <label htmlFor="height">Hauteur : </label>
+          <input
+            id="height"
+            type="number"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <label htmlFor="bombs">Bombes : </label>
+          <input
+            id="bombs"
+            type="number"
+            value={bombs}
+            onChange={(e) => setBombs(e.target.value)}
+          />
+        </InputWrapper>
       </div>
-    </div>
+      <Plate width={width} height={height} bombs={bombs} />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-family: "Poppins", sans-serif;
+  height: 100vh;
+  width: 100%;
+  background-color: #fff;
+`;
+
+const InputWrapper = styled.div`
+  margin: 0 8px;
+`;
 
 export default App;
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
+import bomb from "./utils/bomb.png";
+import flag from "./utils/flag.png";
 
 const Cell = ({ index, cell, onDiscover, onFlag }) => {
   const handleDiscover = (index) => {
@@ -19,14 +20,48 @@ const Cell = ({ index, cell, onDiscover, onFlag }) => {
       isBomb={cell.status === "BOMB"}
       isDiscovered={cell.discover}
       isFlagged={cell.flagged}
+      nb={cell.status}
     >
-      <span>
-        {cell.status !== "BOMB"
-          ? `${cell.status === 0 ? "" : cell.status}`
-          : "B"}
-      </span>
+      <p>{getCell(cell.flagged ? "FLAG" : cell.status)}</p>
     </StyledCell>
   );
+};
+
+const cligno = keyframes`
+  0%{
+    background-color: #e86a6a;
+  }
+  100%{
+    background-color: pink;
+  }
+`;
+
+const getCell = (status) => {
+  switch (status) {
+    case "BOMB":
+      return <img src={bomb}></img>;
+    case "FLAG":
+      return <img src={flag}></img>;
+    case 0:
+      return;
+    default:
+      return `${status}`;
+  }
+};
+
+const getColor = (i) => {
+  const colors = [
+    "#6ed63a",
+    "#af1160",
+    "#ff4524",
+    "#cfb11b",
+    "#2f5c83",
+    "#27b173",
+    "#0b0c60",
+    "#786255",
+  ];
+
+  return colors[i];
 };
 
 const StyledCell = styled.div`
@@ -36,24 +71,41 @@ const StyledCell = styled.div`
   height: 25px;
   width: 25px;
   user-select: none;
-  box-shadow: 0 0 0 1px #2e2e2e;
-  background-color: grey;
+  box-shadow: 0 0 0 1px #ffffff;
+  background-color: #ffb8ca;
+  font-weight: 600;
+  color: ${(p) => getColor(p.nb)};
   cursor: pointer;
-  span {
+  &:hover {
+    animation: ${cligno} 1s infinite alternate;
+  }
+  p {
     display: none;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    padding: 2px;
+    box-sizing: border-box;
+    img {
+      height: 100%;
+      width: auto;
+    }
   }
   ${(p) =>
     p.isDiscovered &&
     css`
-      background-color: ${p.isBomb ? "red" : "lightgreen"};
-      span {
-        display: block;
+      background-color: ${p.isBomb ? "#e65252" : "#ffe5e5"};
+      p {
+        display: flex;
       }
     `}
   ${(p) =>
     p.isFlagged &&
     css`
-      background-color: orange;
+      p {
+        display: flex;
+      }
     `}
 `;
 
